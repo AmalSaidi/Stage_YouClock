e37b0206f5b1be4471dd9eee998187f158856976
Amal SAIDI <saidiamal300@gmail.com>
2022-07-29 11:57:59 +0200

    ajout des migrations et seeds

--- database/migrations/2022_07_28_215635_create_semainetypes_table.php
+++ database/migrations/2022_07_28_215635_create_semainetypes_table.php
@@ -17,13 +17,13 @@ class CreateSemainetypesTable extends Migration
             $table->id();
             $table->string('iduser');
             $table->string('jour');
-            $table->float('poidsJour')->nullable();;
-            $table->time('DM')->nullable();;
-            $table->time('FM')->nullable();;
-            $table->time('DA')->nullable();;
-            $table->time('FA')->nullable();;
-            $table->time('DS')->nullable();;
-            $table->time('FS')->nullable();;
+            $table->float('poidsJour')->nullable();
+            $table->time('DM')->nullable();
+            $table->time('FM')->nullable();
+            $table->time('DA')->nullable();
+            $table->time('FA')->nullable();
+            $table->time('DS')->nullable();
+            $table->time('FS')->nullable();
         });
     }
 
