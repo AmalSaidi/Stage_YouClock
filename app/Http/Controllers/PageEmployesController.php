315f874d3d7fecb74283fcdb8fca24b3439e35a8
Amal SAIDI <saidiamal300@gmail.com>
2022-07-29 11:38:56 +0200

    ajout des migrations et seeds

--- app/Http/Controllers/PageEmployesController.php
+++ app/Http/Controllers/PageEmployesController.php
@@ -80,7 +80,7 @@ class PageEmployesController extends Controller
         $session_str = $user->structure;
         $employees = DB::table('employes')->where('structure', 'like', '%'.$session_str.'%')->where('admin',0)->get();
         $employes = DB::select('select * from employes where id = ?',[$id]);
-        $fiche = DB::select('select DISTINCT idfiche,statutF from fichehors where idUser = (select identifiant from employes where id = ?) ORDER BY id DESC LIMIT 1',[$id]);
+        $fiche = DB::select('select idfiche,statutF from fichehors where idUser = (select identifiant from employes where id = ?) ORDER BY id DESC LIMIT 1',[$id]);
         $Lun= DB::select('select * from semainetypes where jour="Lundi" and idUser=(select identifiant from employes where id=?)',[$id]);
         $Mar= DB::select('select * from semainetypes where jour="Mardi" and idUser=(select identifiant from employes where id=?)',[$id]);
         $Mer= DB::select('select * from semainetypes where jour="Mercredi" and idUser=(select identifiant from employes where id=?)',[$id]);
