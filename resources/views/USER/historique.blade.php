95b2da01dabc46837331585cfaca2329b62e6e8c
Amal SAIDI <saidiamal300@gmail.com>
2022-08-02 03:03:31 +0200

    change pics

--- resources/views/USER/historique.blade.php
+++ resources/views/USER/historique.blade.php
@@ -100,7 +100,7 @@ $Valide="Valide";
  <option value="12">Décembre</option>
  </select>
   </div>
-  <button type="submit" class="btn btn-primary" id="ajouterr-button">AJOUTER</button>
+  <button type="submit" class="btn btn-primary" id="add">AJOUTER</button>
 </form>
   </div>
 
